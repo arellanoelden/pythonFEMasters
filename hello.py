0affdbbbe49cad39a441dd6a6a2bf7df86f93a15
arellanoelden <arellanoelden11@yahoo.com>
2020-01-23 19:37:39 -0800

    day one from py workshop

---
+++ hello.py
@@ -0,0 +1,4 @@
+greetings = ["hello", "Bonjour", "Hola"]
+
+for greeting in greetings:
+    print(f"{greeting}, World!")
---
+++ name_lib.py
@@ -0,0 +1,8 @@
+def upper_case_name(name):
+    return name.upper()
+
+
+if __name__ == "__main__":
+    name = "Elden"
+    upper_name = upper_case_name(name)
+    print(f"Upper cased name: {upper_name}")
